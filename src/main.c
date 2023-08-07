221e23bfe65dc176629f317985f35e02fa567e40
koriori <maaacha.kuri05@gmail.com>
2023-08-07 17:31:20 +0900

    update: 方向は違うけど前後両方に動くようにする

--- src/main.c
+++ src/main.c
@@ -15,7 +15,7 @@ int	close_game(t_game *game)
 
 int input_key(int keycode, t_game *game)
 {
-	printf("position :%lf %lf + %lf\n", game->player->position_x, game->player->position_y, game->player->direction_x * game->player->move_speed);
+	printf("before position :%lf %lf + %lf\n", game->player->position_x, game->player->position_y, game->player->direction_x * game->player->move_speed);
 	if (keycode == KEY_ESC)
 		close_game(game);
 	else if (keycode == KEY_UP || keycode == KEY_W)
@@ -24,14 +24,14 @@ int input_key(int keycode, t_game *game)
 		if (game->world_map[(int)(game->player->position_x + game->player->direction_x * game->player->move_speed)][(int)(game->player->position_y)] == 0 || ft_strchr("NSWE", game->world_map[(int)(game->player->position_x + game->player->direction_x * game->player->move_speed)][(int)(game->player->position_y)]))
 			game->player->position_x += game->player->direction_x * game->player->move_speed;
 		if (game->world_map[(int)(game->player->position_x)][(int)(game->player->position_y + game->player->direction_y * game->player->move_speed)] == 0 || ft_strchr("NSWE", game->world_map[(int)(game->player->position_x)][(int)(game->player->position_y + game->player->direction_y * game->player->move_speed)]))
-			game->player->position_y += game->player->direction_y * game->player->move_speed;		
+			game->player->position_y += game->player->direction_y * game->player->move_speed;
 	}
 	else if (keycode == KEY_DOWN || keycode == KEY_S)
 	{
 		printf("back\n");
-		if (!game->world_map[(int)(game->player->position_x - game->player->direction_x * game->player->move_speed)][(int)(game->player->position_y)])
+		if (!game->world_map[(int)(game->player->position_x - game->player->direction_x * game->player->move_speed)][(int)(game->player->position_y)] || ft_strchr("NSWE", game->world_map[(int)(game->player->position_x + game->player->direction_x * game->player->move_speed)][(int)(game->player->position_y)]))
 			game->player->position_x -= game->player->direction_x * game->player->move_speed;
-		if (!game->world_map[(int)(game->player->position_x)][(int)(game->player->position_y - game->player->direction_y * game->player->move_speed)])
+		if (!game->world_map[(int)(game->player->position_x)][(int)(game->player->position_y - game->player->direction_y * game->player->move_speed)] || ft_strchr("NSWE", game->world_map[(int)(game->player->position_x)][(int)(game->player->position_y + game->player->direction_y * game->player->move_speed)]))
 			game->player->position_y -= game->player->direction_y * game->player->move_speed;
 	}
 	else if (keycode == KEY_RIGHT || keycode == KEY_D)
@@ -54,7 +54,8 @@ int input_key(int keycode, t_game *game)
 		game->player->plane_x = game->player->plane_x * cos(game->player->rot_speed) - game->player->plane_y * sin(game->player->rot_speed);
 		game->player->plane_y = old_plane_x * sin(game->player->rot_speed) + game->player->plane_y * cos(game->player->rot_speed);
 	}
-	printf("position :%lf %lf\n", game->player->position_x, game->player->position_y);
+	printf("after  position :%lf %lf\n", game->player->position_x, game->player->position_y);
+	// update_map(game);
 	return (0);
 }
 
