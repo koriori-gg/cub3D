2185f78f050876c2c3e21b829997962282f11019
koriori <maaacha.kuri05@gmail.com>
2023-08-07 14:02:58 +0900

    refactor: ddaとdrawをポインタで受け渡す

--- src/raycasting/calculate.c
+++ src/raycasting/calculate.c
@@ -155,43 +155,46 @@ void	prepare_map_draw(t_draw	*draw, double perp_wall_dist)
 	draw->draw_end = calculate_draw_end(height, draw->line_height);
 }
 
-int calculate_texture_x(t_game *game,t_dda dda, int side)
+int calculate_texture_x(t_game *game,t_dda *dda)
 {
 	double wall_x;
 	int tex_x;
 
-	if (side == 0)
-		wall_x = game->player->position_y + dda.perp_wall_dist * dda.ray_direction_y;
+	if (dda->side == 0)
+		wall_x = game->player->position_y + dda->perp_wall_dist * dda->ray_direction_y;
 	else
-		wall_x = game->player->position_x + dda.perp_wall_dist * dda.ray_direction_x;
+		wall_x = game->player->position_x + dda->perp_wall_dist * dda->ray_direction_x;
 	wall_x -= floor(wall_x);
 	tex_x = (int)(wall_x * (double)tex_width);
-	if (side == 0 && dda.ray_direction_x > 0)
+	if (dda->side == 0 && dda->ray_direction_x > 0)
 		tex_x = tex_width - tex_x - 1;
-	if (side == 1 && dda.ray_direction_y < 0)
+	if (dda->side == 1 && dda->ray_direction_y < 0)
 		tex_x = tex_width - tex_x - 1;
 	return (tex_x);
 }
 
-void	save_color(t_game *game, t_dda dda, t_draw draw, int x)
+void	save_color(t_game *game, t_dda *dda, t_draw *draw, int x)
 {
-		draw.tex_num = char_to_int(game->world_map[dda.map_x][dda.map_y]);//もうちょっといい変換方法ありそう
-		draw.tex_x = calculate_texture_x(game, dda, dda.side);
-		draw.step = 1.0 * tex_height / draw.line_height;
-		draw.tex_position = (draw.draw_start - height / 2 + draw.line_height / 2) * draw.step;
-		int y;
-		y = draw.draw_start;
-		while (y < draw.draw_end)
-		{
-			int tex_y = (int)draw.tex_position & (tex_height - 1);
-			draw.tex_position += draw.step;
-			int color = game->texture[draw.tex_num][tex_height * tex_y + draw.tex_x];
-			if (dda.side == 1)
-				color = (color >> 1) & 8355711;
-			game->buf[y][x] = color;
-			game->re_buf = 1;
-			y++;
-		}
+	int y;
+	int color;
+	int tex_y;
+
+	draw->tex_num = char_to_int(game->world_map[dda->map_x][dda->map_y]);//もうちょっといい変換方法ありそう
+	draw->tex_x = calculate_texture_x(game, dda);
+	draw->step = 1.0 * tex_height / draw->line_height;
+	draw->tex_position = (draw->draw_start - height / 2 + draw->line_height / 2) * draw->step;
+	y = draw->draw_start;
+	while (y < draw->draw_end)
+	{
+		tex_y = (int)draw->tex_position & (tex_height - 1);
+		draw->tex_position += draw->step;
+		color = game->texture[draw->tex_num][tex_height * tex_y + draw->tex_x];
+		if (dda->side == 1)
+			color = (color >> 1) & 8355711;
+		game->buf[y][x] = color;
+		game->re_buf = 1;
+		y++;
+	}
 }
 
 void	calculate(t_game *game)
@@ -213,7 +216,7 @@ void	calculate(t_game *game)
 			dda.perp_wall_dist = (dda.map_y - game->player->position_y + (1 - dda.step_y) / 2) / dda.ray_direction_y;
 		prepare_map_draw(&draw, dda.perp_wall_dist);
 
-		save_color(game, dda, draw, x);
+		save_color(game, &dda, &draw, x);
 		x++;
 	}
 }
