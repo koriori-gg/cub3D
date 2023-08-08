ddb667ad75709207e0bd314929b1387bff82e217
koriori <maaacha.kuri05@gmail.com>
2023-08-08 09:38:18 +0900

    add: map更新用関数の追加

--- src/main.c
+++ src/main.c
@@ -21,6 +21,54 @@ void	update_pre_position(int keycode, t_game *game)
 		game->player->pre_position_y = game->player->position_y;
 }
 
+void	change_map(t_game *game, t_map *map, int map_x, int map_y)
+{
+	char	temp;
+	int		pre_map_x;
+	int		pre_map_y;
+
+	temp = game->world_map[map_y][map_x];
+	pre_map_x = (int)game->player->pre_position_x;
+	pre_map_y = (int)game->player->pre_position_y;
+	while (map->y < map_y)
+		map = map->next;
+	while (map->y < pre_map_y)
+		map = map->next;
+	if (map->row[map_x] == 0)
+	{
+		temp = map->row[map_x];
+		map->row[map_x] = map->row[pre_map_x];
+		map->row[pre_map_x] = temp;
+	}
+}
+
+void	change_worldmap(int map_x, int map_y, t_game *game)
+{
+	char	temp;
+	int		pre_map_x;
+	int		pre_map_y;
+
+	if (game->world_map[map_y][map_x] == 0)
+	{
+		temp = game->world_map[map_y][map_x];
+		pre_map_x = (int)game->player->pre_position_x;
+		pre_map_y = (int)game->player->pre_position_y;
+		game->world_map[map_y][map_x] = game->world_map[pre_map_y][pre_map_x];
+		game->world_map[pre_map_y][pre_map_x] = temp;
+	}
+}
+
+void	update_map(t_game *game)
+{
+	int	map_x;
+	int map_y;
+
+	map_x = (int)game->player->position_x;
+	map_y = (int)game->player->position_y;
+	change_map(game, game->map_info->map, map_x, map_y);
+	change_worldmap(map_x, map_y, game);
+}
+
 int input_key(int keycode, t_game *game)
 {
 	printf("before position :x %lf y %lf + %lf\n", game->player->position_x, game->player->position_y, game->player->direction_x * game->player->move_speed);
@@ -64,7 +112,7 @@ int input_key(int keycode, t_game *game)
 		game->player->plane_y = old_plane_x * sin(game->player->rot_speed) + game->player->plane_y * cos(game->player->rot_speed);
 	}
 	printf("after  position :x %lf y %lf\n", game->player->position_x, game->player->position_y);
-	// update_map(game);
+	update_map(game);
 	return (0);
 }
 
