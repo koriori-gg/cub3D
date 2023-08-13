507c065cfe858f3e83de69b8fc8c34a42d7f0cdb
Sota Okuno <sokuno@c1r23s1.42tokyo.jp>
2023-08-13 14:01:59 +0900

    feat: ファイルの拡張子が.cubかどうかを確認する関数を作成

--- include/cub3d.h
+++ include/cub3d.h
@@ -1,9 +1,15 @@
 #ifndef CUB3D_H
 # define CUB3D_H
 
-#include "ft_printf.h"
-#include "get_next_line.h"
-#include "libft.h"
-#include "mlx.h"
+# include "ft_printf.h"
+# include "get_next_line.h"
+# include "libft.h"
+# include "mlx.h"
+# include <stdbool.h>
+
+typedef struct s_cub_info {
+	char				*line;
+	struct s_cub_info	*next;	
+}	t_cub_info;
 
 #endif
--- src/main.c
+++ src/main.c
@@ -7,9 +7,38 @@ void	exit_with_error_message(char *message)
 	exit(1);
 }
 
+bool	is_correct_filename(char *path)
+{
+	int	path_len;
+
+	if (!path)
+		return (false);
+	path_len = ft_strlen(path);
+	if (ft_strncmp(&path[path_len - 4], ".cub", 4))
+		return (false);
+	return (true);
+}
+
+
+// ファイルがあるかどうか
+// ファイルの読み取り権限があるかどうか
+// ファイルの拡張子が.cubかどうか
+t_cub_info *read_cub(char *path)
+{
+	t_cub_info *cub_info = NULL;
+
+	if (!is_correct_filename(path))
+		exit_with_error_message("invalid filename");
+	
+	return (cub_info);
+}
+
 int main(int argc, char *argv[])
 {
-	(void)argv;
+	t_cub_info	*cub_info;
+
 	if (argc != 2)
 		exit_with_error_message("invalid input");
+	cub_info = read_cub(argv[1]);
+	return (0);
 }
