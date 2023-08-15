d5df09c2807e295389799c1753a96b0a513bdb5c
Sota Okuno <sokuno@c1r23s1.42tokyo.jp>
2023-08-15 17:00:34 +0900

    fix: example.cubのマップをpdf通りに変更

--- include/cub3d.h
+++ include/cub3d.h
@@ -21,4 +21,8 @@ void	exit_with_error(char *message);
 t_cub_info read_cub(char *path);
 void	free_double_pointer(void *ptr);
 
+// debug
+void	print_info(t_cub_info info);
+void	print_two_dimensional_array(char **array);
+
 #endif
