3b5023502f342544edbbd8ba510769b7437ae15b
Sota Okuno <sokuno@c1r23s1.42tokyo.jp>
2023-08-20 22:10:08 +0900

    feat: プレイヤーが行ける範囲を2で埋める

--- src/validate_map_info.c
+++ src/validate_map_info.c
@@ -31,28 +31,100 @@ int	check_object(char object)
 	return (0);
 }
 
-// 壁に囲まれているか
+char	**make_map_copy(char **map)
+{
+	char	**map_copy;
+	int		i;
+	int		height;
+
+	i = 0;
+	while (map[i])
+		i++;
+	height = i;
+	map_copy = calloc(height + 1, sizeof(char *));
+	if (!map_copy)
+		return (NULL);
+	i = 0;
+	while (i < height)
+	{
+		map_copy[i] = ft_strdup(map[i]);
+		if (!map_copy[i])
+			return (NULL);
+		i++;
+	}
+	return (map_copy);
+}
+
+// 行こうとしてるところのlenとって確保してない領域にアクセスしないようにするのよさそう
+void	search_reachable_squares(char **map, int i, int j)
+{
+	if (j >= (int)ft_strlen(map[i]) || map[i][j] == ' ')
+		exit_with_error("gomimap\n");
+	if (map[i][j] == '1' || map[i][j] == '2')
+		return ;
+	map[i][j] = '2';
+	search_reachable_squares(map, i, j + 1);
+	search_reachable_squares(map, i + 1, j);
+	search_reachable_squares(map, i, j - 1);
+	search_reachable_squares(map, i - 1, j);
+}
+
+void	check_wall(char **map, int y, int x)
+{
+	char	**map_copy;
+	int		i;
+	int		height;
+
+	map_copy = make_map_copy(map);
+	search_reachable_squares(map_copy, y, x);
+	print_two_dimensional_array(map_copy);
+	i = 0;
+	while (map[i])
+		i++;
+	height = i;
+	i = 0;
+	while (i < height)
+	{
+		if (ft_strchr(map_copy[i], 'C'))
+		{
+			free_double_pointer(map_copy);
+			return ;
+		}
+		i++;
+	}
+	i = 0;
+	free_double_pointer(map_copy);
+}
+
+// 壁に囲まれているか 斜めもダメにする
 // 01NSEWのみで構成されているか
 static void	validate_map(char **map)
 {
 	int	count;
 	int	i;
 	int	j;
+	int x, y;
 
 	count = 0;
 	i = 0;
 	while (map[i])
 	{
 		j = 0;
-		while (map[i][j] != '\n' && map[i][j] != '\0')
+		while (map[i][j] != '\0')
 		{
-			count += check_object(map[i][j]);
+			if (check_object(map[i][j]))
+			{
+				x = j;
+				y = i;
+				count++;
+			}
 			j++;
 		}
 		i++;
 	}
 	if (count != 1)
 		exit_with_error("too many players\n");
+	check_wall(map, y, x);
 }
 
 void	validate_map_info(t_map_info map_info)
