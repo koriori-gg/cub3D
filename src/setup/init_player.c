0dbef4a1891d99a44720a71bf6257a7c16befd95
koriori <maaacha.kuri05@gmail.com>
2023-08-21 23:12:36 +0900

    refactor: norminette用に関数内の書き方を変更

--- src/setup/init_player.c
+++ src/setup/init_player.c
@@ -2,41 +2,36 @@
 
 static void	set_player_direction(t_game *game, char c)
 {
-	if (c == 'N')
+	if (c == 'N' || c == 'S')
 	{
 		game->player->direction_x = 0.0;
 		game->player->direction_y = -1.0;
 		game->player->angle_x = 0.707;
 		game->player->angle_y = 0.0;
-
+		if (c == 'S')
+		{
+			game->player->direction_y = 1.0;
+			game->player->angle_x = -0.707;
+		}
 	}
-	if (c == 'S')
-	{
-		game->player->direction_x = 0.0;
-		game->player->direction_y = 1.0;
-		game->player->angle_x = -0.707;
-		game->player->angle_y = 0.0;
-	}
-	if (c == 'E')
-	{
-		game->player->direction_x = 1.0;
-		game->player->direction_y = 0.0;
-		game->player->angle_x = 0.0;
-		game->player->angle_y = 0.707;
-	}
-	if (c == 'W')
+	if (c == 'E' || c == 'W')
 	{
 		game->player->direction_x = -1.0;
 		game->player->direction_y = 0.0;
 		game->player->angle_x = 0.0;
 		game->player->angle_y = -0.707;
+		if (c == 'E')
+		{
+			game->player->direction_x = 1.0;
+			game->player->angle_y = 0.707;
+		}
 	}
 }
 
 static void	extract_player_info(t_game *game)
 {
 	int	y;
-	int x;
+	int	x;
 
 	y = 0;
 	while (game->map_info.map[y])
