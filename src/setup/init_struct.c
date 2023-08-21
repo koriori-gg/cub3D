e5cc715881398d0623d397818f5e9dc6c54544f4
koriori <maaacha.kuri05@gmail.com>
2023-08-21 09:11:28 +0900

    update: NSEWに応じて初期値を変更する

--- src/setup/init_struct.c
+++ src/setup/init_struct.c
@@ -1,6 +1,39 @@
 #include "cub3d.h"
 
-void	extract_player_position(t_game *game)
+void	set_player_direction(t_game *game, char c)
+{
+	if (c == 'N')
+	{
+		game->player->direction_x = 0.0;
+		game->player->direction_y = -1.0;
+		game->player->plane_x = 0.707;
+		game->player->plane_y = 0.0;
+
+	}
+	if (c == 'S')
+	{
+		game->player->direction_x = 0.0;
+		game->player->direction_y = 1.0;
+		game->player->plane_x = -0.707;
+		game->player->plane_y = 0.0;
+	}
+	if (c == 'E')
+	{
+		game->player->direction_x = 1.0;
+		game->player->direction_y = 0.0;
+		game->player->plane_x = 0.0;
+		game->player->plane_y = 0.707;
+	}
+	if (c == 'W')
+	{
+		game->player->direction_x = -1.0;
+		game->player->direction_y = 0.0;
+		game->player->plane_x = 0.0;
+		game->player->plane_y = -0.707;
+	}
+}
+
+void	extract_player_info(t_game *game)
 {
 	int	y;
 	int x;
@@ -15,6 +48,7 @@ void	extract_player_position(t_game *game)
 			{
 				game->player->position_x = (double)x + 0.5;
 				game->player->position_y = (double)y + 0.5;
+				set_player_direction(game, game->map_info.map[y][x]);
 				game->map_info.map[y][x] = '0';
 				return ;
 			}
@@ -29,11 +63,7 @@ static void	init_player(t_game *game)
 	game->player = (t_player *)ft_calloc(1, sizeof(t_player));
 	if (!game->player)
 		error_exit(MALLOC_ERROR);
-	extract_player_position(game);
-	game->player->direction_x = 0.0;
-	game->player->direction_y = -1.0;
-	game->player->plane_x = 0.66;
-	game->player->plane_y = 0.0;
+	extract_player_info(game);
 	game->player->move_speed = 0.05;
 	game->player->rotation_speed = 0.05;
 }
