b13ea94e6f916f987fd55fd3807c0a16814eec31
Iori Hashimoto <ihashimo@c1r14s9.42tokyo.jp>
2023-08-29 08:17:46 +0900

    update: mlx系列でvoid*を返す関数に関して、エラー処理を追加した関数を追加

---
+++ src/utils/ft_mlx.c
@@ -0,0 +1,51 @@
+#include "cub3d.h"
+
+void	*ft_mlx_init(void)
+{
+	void	*mlx;
+
+	mlx = mlx_init();
+	if (!mlx)
+		exit_with_error("mlx error");
+	return (mlx);
+}
+
+void	*ft_mlx_new_window(void *mlx_ptr, int size_x, int size_y, char *title)
+{
+	void	*win;
+
+	win = mlx_new_window(mlx_ptr, size_x, size_y, title);
+	if (!win)
+		exit_with_error("mlx error");
+	return (win);
+}
+
+void	*ft_mlx_new_image(void *mlx_ptr, int width, int height)
+{
+	void	*img;
+
+	img = mlx_new_image(mlx_ptr, width, height);
+	if (!img)
+		exit_with_error("mlx error");
+	return (img);
+}
+
+void	*ft_mlx_xpm_file_to_image(void *mlx_ptr, char *filename, int *width, int *height)
+{
+	void	*img;
+
+	img = mlx_xpm_file_to_image(mlx_ptr, filename, width, height);
+	if (!img)
+		exit_with_error("mlx error");
+	return (img);
+}
+
+char	*ft_mlx_get_data_addr(void *img_ptr, int *bits_per_pixel, int *size_line, int *endian)
+{
+	char	*data;
+
+	data = mlx_get_data_addr(img_ptr, bits_per_pixel, size_line, endian);
+	if (!data)
+		exit_with_error("mlx error");
+	return (data);
+}
